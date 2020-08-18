4c6e0ac13436acc19f9e13126662374a3e754a0d
Faviola Caballero <fabycg18@gmail.com>
2020-08-18 13:49:11 -0700

    changed and added images also added a button

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
-const firstImage = "url(\"https://images.unsplash.com/photo-1491466424936-e304919aada7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60\")";
-const secondImage = "url(\"https://images.unsplash.com/photo-1472213984618-c79aaec7fef0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60\")";
-const thirdImage = "url(\"https://images.unsplash.com/photo-1419064642531-e575728395f2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60\")";
-
-const imageGallery = [firstImage, secondImage, thirdImage];
+const firstImage = "url(\"https://images.pexels.com/photos/2104742/pexels-photo-2104742.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const secondImage = "url(\"https://images.pexels.com/photos/6618/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const thirdImage = "url(\"https://images.pexels.com/photos/87501/pexels-photo-87501.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const fourthImage = "url(\"https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const fifthImage = "url(\"https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const sixthImage = "url(\"https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
+const imageGallery = [firstImage, secondImage, thirdImage, fourthImage, fifthImage, sixthImage];
 
 let targetIndex = 0;
 
 setInterval(() => {
     document.getElementsByClassName("back-img")[0].style.backgroundImage = imageGallery[targetIndex];
     
-    if (targetIndex === 2) {
+    if (targetIndex === 4) {
         targetIndex = 0;
     } else {
         targetIndex++;
