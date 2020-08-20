35428048b96eab787ef339bba3ae6ba9a1f9eb8c
Faviola Caballero <fabycg18@gmail.com>
2020-08-19 23:04:31 -0700

    changed images

--- js/index.js
+++ js/index.js
@@ -1,17 +1,20 @@
-const firstImage = "url(\"https://images.pexels.com/photos/2104742/pexels-photo-2104742.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const secondImage = "url(\"https://images.pexels.com/photos/6618/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const thirdImage = "url(\"https://images.pexels.com/photos/87501/pexels-photo-87501.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const fourthImage = "url(\"https://images.pexels.com/photos/2166553/pexels-photo-2166553.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const fifthImage = "url(\"https://images.pexels.com/photos/1603650/pexels-photo-1603650.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const sixthImage = "url(\"https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500\")";
-const imageGallery = [firstImage, secondImage, thirdImage, fourthImage, fifthImage, sixthImage];
+const firstImage = "url(\"https://st2.depositphotos.com/2058297/6457/v/450/depositphotos_64576809-stock-illustration-san-francisco-skyline.jpg\")";
+const secondImage = "url(\"https://newcastlebeach.org/images/london-cartoon-8.png\")";
+const thirdImage = "url(\"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/254f318f-c1bd-4166-88a9-7986e45bb4fe/dbvhka5-93e03bed-ea67-4f71-851b-daf8b61a39d1.png/v1/fill/w_1024,h_727,strp/beutiful_sunset_in_desert_mosque_silhouette_by_simplexor_dbvhka5-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD03MjciLCJwYXRoIjoiXC9mXC8yNTRmMzE4Zi1jMWJkLTQxNjYtODhhOS03OTg2ZTQ1YmI0ZmVcL2RidmhrYTUtOTNlMDNiZWQtZWE2Ny00ZjcxLTg1MWItZGFmOGI2MWEzOWQxLnBuZyIsIndpZHRoIjoiPD0xMDI0In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.aJyfwsLYewgwSIKG9h43x552jWQ-XhkpYiAHPeJwp2U\")";
+const fourthImage = "url(\"https://static.vecteezy.com/system/resources/thumbnails/000/274/410/original/Paris_Illustration.jpg\")";
+const fifthImage = "url(\"https://i.pinimg.com/originals/90/d1/77/90d177942abcd783dea3b29ba00f32f3.jpg\")";
+const sixthImage = "url(\"https://images.alphacoders.com/961/961608.jpg\")";
+const seventhImage = "url(\"https://images6.alphacoders.com/887/887558.jpg\")";
+const eightImage = "url(\"https://images6.alphacoders.com/102/1026927.jpg\")";
+
+const imageGallery = [firstImage, secondImage, thirdImage, fourthImage, fifthImage, sixthImage, seventhImage, eightImage];
 
 let targetIndex = 0;
 
 setInterval(() => {
     document.getElementsByClassName("back-img")[0].style.backgroundImage = imageGallery[targetIndex];
     
-    if (targetIndex === 4) {
+    if (targetIndex === 7) {
         targetIndex = 0;
     } else {
         targetIndex++;
